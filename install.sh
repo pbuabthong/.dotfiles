baf4b996e4d543cd6f769058367b682229ce07c9
Pai Buabthong <pbuabthong@caltech.edu>
2019-03-03 00:24:15 -0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "so ~/.dotfiles/vim/.vimrc" >> ~/.vimrc
+echo "source-file ~/.dotfiles/tmux/.tmux.conf" >> ~/.tmux.conf
+echo "source ~/.dotfiles/shell/.dotfiles_alias" >> ~/.bash_profile
